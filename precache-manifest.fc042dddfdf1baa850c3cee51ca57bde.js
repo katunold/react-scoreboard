a5b25a7dc426a4ebfbca29868da3f1eb548b6304
katunold <katunold94@gmail.com>
2019-07-24 17:10:57 +0300

    Updates

---
+++ precache-manifest.fc042dddfdf1baa850c3cee51ca57bde.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "391f4dcc93e6de3dd3b79d380a431e1b",
+    "url": "/react-scoreboard/index.html"
+  },
+  {
+    "revision": "22a468f5ebda5f411c03",
+    "url": "/react-scoreboard/static/css/main.d92f6448.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/react-scoreboard/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "22a468f5ebda5f411c03",
+    "url": "/react-scoreboard/static/js/main.0c3fbf05.chunk.js"
+  },
+  {
+    "revision": "da4941b842451d92ab2c",
+    "url": "/react-scoreboard/static/js/runtime~main.d9728706.js"
+  }
+]);
